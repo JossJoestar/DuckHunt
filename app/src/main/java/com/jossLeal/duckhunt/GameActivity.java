76803ee191f04b330bed72a10d0b59b9a3f7a3b5
Oreos <oreoslawiet@gmail.com>
2020-07-22 16:13:04 -0700

    Counter

--- app/src/main/java/com/jossLeal/duckhunt/GameActivity.java
+++ app/src/main/java/com/jossLeal/duckhunt/GameActivity.java
@@ -4,20 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jossLeal.duckhunt.common.Constantes;
 
 public class GameActivity extends AppCompatActivity {
 	TextView tvCounterDucks, tvTimer, tvNick;
+	ImageView ivDuck;
+	int counter = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		initViewComponets();
+		eventos();
+	}
+	
+	private void initViewComponets(){
 		tvCounterDucks = findViewById(R.id.textViewCounter);
 		tvTimer = findViewById(R.id.textViewTimer);
 		tvNick = findViewById(R.id.textViewNick);
+		ivDuck = findViewById(R.id.imageViewDuck);
 		
 		// Asignar Fuentes personalizadas
 		Typeface typeface = Typeface.createFromAsset(getAssets(), "pixel.ttf");
@@ -29,5 +39,14 @@ public class GameActivity extends AppCompatActivity {
 		Bundle extras = getIntent().getExtras();
 		String nick = extras.getString(Constantes.EXTRA_NICK);
 		tvNick.setText(nick);
+	}
+	private void eventos() {
+		ivDuck.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				counter++;
+				tvCounterDucks.setText(String.valueOf(counter));
+			}
+		});
 	}
 }
